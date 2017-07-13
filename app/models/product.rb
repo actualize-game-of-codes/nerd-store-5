97311cdac37b1bbf06087b48418095d937300679
Peter Jang <peter.x.jang@gmail.com>
2017-07-13 18:16:19 -0500

    Refactor code to move view stuff into the HTML instead of the model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,4 @@
 class Product < ActiveRecord::Base
-  def sale_message
-    if price.to_f < 100
-      "Discount item!"
-    else
-      "Everyday value!"
-    end
-  end
-
   def tax
     price.to_f * 0.09
   end
@@ -15,11 +7,7 @@ class Product < ActiveRecord::Base
     price.to_f + tax
   end
 
-  def sale_message_class_name
-    if sale_message == "Discount item!"
-      return "discount-item"
-    else
-      return ""
-    end
+  def discounted?
+    price.to_f < 100
   end
 end
