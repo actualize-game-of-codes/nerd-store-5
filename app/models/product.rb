6cf5a5df1c77bcc0b5ddd8b3438bf28fe46ecb4b
Peter Jang <peter.x.jang@gmail.com>
2017-07-13 21:08:03 -0500

    Remove float conversions and format price on index page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ActiveRecord::Base
   def tax
-    price.to_f * 0.09
+    price * 0.09
   end
 
   def total
-    price.to_f + tax
+    price + tax
   end
 
   def discounted?
-    price.to_f < 100
+    price < 100
   end
 end
