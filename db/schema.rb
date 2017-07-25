91e055d192a8a296a710b13b3086cdbb9d300336
Peter Jang <peter.x.jang@gmail.com>
2017-07-24 19:46:27 -0500

    Fix missing quantity attribute on orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724231048) do
+ActiveRecord::Schema.define(version: 20170725004556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170724231048) do
     t.decimal  "total",      precision: 9, scale: 2
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
+    t.integer  "quantity"
   end
 
   create_table "products", force: :cascade do |t|
