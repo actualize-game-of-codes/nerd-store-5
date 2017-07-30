7b8530596c91ebcabde23d56ec569a846f926a1d
Peter Jang <peter.x.jang@gmail.com>
2017-07-30 09:39:04 -0500

    Add shopping cart create and index actions

---
+++ app/controllers/carted_products_controller.rb
@@ -0,0 +1,17 @@
+class CartedProductsController < ApplicationController
+  def index
+    @carted_products = current_user.carted_products.where(status: "carted")
+    render "index.html.erb"
+  end
+
+  def create
+    carted_product = CartedProduct.new(
+      user_id: current_user.id,
+      product_id: params[:form_product_id],
+      quantity: params[:form_quantity],
+      status: "carted"
+    )
+    carted_product.save
+    redirect_to "/carted_products"
+  end
+end
