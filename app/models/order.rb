8b4fb266d677339ff562644dbf2f7b4667b4572f
Peter Jang <peter.x.jang@gmail.com>
2017-07-30 09:55:13 -0500

    Add ability to purchase items in shopping cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,10 @@ class Order < ApplicationRecord
   has_many :carted_products
   has_many :products, through: :carted_products
 
-  def calculate_subtotal_tax_and_total
-    calculated_subtotal = quantity * product.price
-    calculated_tax = quantity * product.tax
-    calculated_total = calculated_subtotal + calculated_tax
-    update(subtotal: calculated_subtotal, tax: calculated_tax, total: calculated_total)
-  end
+  # def calculate_subtotal_tax_and_total
+  #   calculated_subtotal = quantity * product.price
+  #   calculated_tax = quantity * product.tax
+  #   calculated_total = calculated_subtotal + calculated_tax
+  #   update(subtotal: calculated_subtotal, tax: calculated_tax, total: calculated_total)
+  # end
 end
