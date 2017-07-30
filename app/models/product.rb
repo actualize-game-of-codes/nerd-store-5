94dfbf405f1a0da8c257245f4618070200c7e421
Peter Jang <peter.x.jang@gmail.com>
2017-07-30 09:23:38 -0500

    Add carted product model and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ActiveRecord::Base
   has_many :category_products
   has_many :categories, through: :category_products
   has_many :images
-  has_many :orders
+  has_many :carted_products
+  has_many :orders, through: :carted_products
   belongs_to :supplier
   # def supplier
   #   return Supplier.find_by(id: supplier_id)
